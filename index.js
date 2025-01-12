cc868f7c6ca18d9f75fb2de99f674799a71067b8
masud-1971 <msd.rana.2008@gmail.com>
2025-01-12 15:22:30 +0600

    Update index.js
    update

--- index.js
+++ index.js
@@ -1,20 +1,42 @@
 const http = require("http");
 const server = http.createServer((req, res) => {
-    if (req.url === "/") {
-        res.end("Home Page");
-    }
-    if (req.url === "/login") {
-        res.end("Login Page");
-    }
+    // if (req.url === "/") {
+    //     res.end("Home Page");
+    // }
+    // if (req.url === "/login") {
+    //     res.end("Login Page");
+    // }
+    //
+    // if (req.url === "/register") {
+    //     res.end("Register Page");
+    // }
+    // if (req.url === "/contact") {
+    //     res.end("Contact Page");
+    // }
 
-    if (req.url === "/register") {
-        res.end("Register Page");
+    res.setHeader("MERN_STACK", "Batch#8");
+
+    let method = req.method;
+    if (method === "GET") {
+        res.end("GET Request");
     }
-    if (req.url === "/contact") {
-        res.end("Contact Page");
+    if (method === "POST") {
+        res.end("POST Request");
+    }
+    if (method === "PUT") {
+        res.end("PUT Request");
+    }
+    if (method === "DELETE") {
+        res.end("DELETE Request");
+    }
+    if (method === "PATCH") {
+        res.end("PATCH Request");
+    }
+    if (method === "PUT") {
+        res.end("PUT Request");
     }
 })
 
-server.listen(8080, function (){
-    console.log("Server is running on port 8080");
+server.listen(8081, function (){
+    console.log("Server is running on port 8081");
 });
